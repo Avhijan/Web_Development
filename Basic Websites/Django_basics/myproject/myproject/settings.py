3843d5e3bcbf744b12df9c04b4467b007ce491a0
Avhijan <abhijansap06@gmail.com>
2026-02-06 23:22:11 +0545

    Allow all hosts in Django settings

--- Basic Websites/Django_basics/myproject/myproject/settings.py
+++ Basic Websites/Django_basics/myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4-7e$#u4&t3ng)r#=fal*sv#$)k4xase-c%5=)5p%g1b(@337%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
